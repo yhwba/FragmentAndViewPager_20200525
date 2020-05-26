6d72b7d8082c8c226cba8ef1a5295020482c5a8b
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-26 10:09:06 +0900

    뷰페이지 + 페이지 설정

--- app/src/main/java/kr/co/yhw/fragmentandviewpager/MainActivity.java
+++ app/src/main/java/kr/co/yhw/fragmentandviewpager/MainActivity.java
@@ -30,5 +30,7 @@ public class MainActivity extends BaseActivity {
     public void setValues() {
         mvpa = new MyViewPagerAdapter(getSupportFragmentManager());
         binding.myViewPager.setAdapter(mvpa);
+
+        binding.myTabLayout.setupWithViewPager(binding.myViewPager);
     }
 }
--- app/src/main/java/kr/co/yhw/fragmentandviewpager/adapters/MyViewPagerAdapter.java
+++ app/src/main/java/kr/co/yhw/fragmentandviewpager/adapters/MyViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package kr.co.yhw.fragmentandviewpager.adapters;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -12,6 +13,21 @@ import kr.co.yhw.fragmentandviewpager.Fragment.ThirdFragment;
 public class MyViewPagerAdapter extends FragmentPagerAdapter {
     public MyViewPagerAdapter(@NonNull FragmentManager fm) {
         super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position ==0){
+            return "첫번째";
+        }
+        else if(position ==1){
+            return "두번째";
+        }
+        else {
+            return "세번째";
+        }
+
     }
 
     @NonNull
